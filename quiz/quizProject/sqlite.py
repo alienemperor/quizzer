636983972271e59448471c2420cc65a609875774
alienemperor <32971607+alienemperor@users.noreply.github.com>
2018-03-02 09:49:06 -0700

    .

--- quiz/quizProject/sqlite.py
+++ quiz/quizProject/sqlite.py
@@ -1,5 +1,5 @@
 import sqlite3
-conn = sqlite3.connect('todo.db') # Warning: This file is created in the current directory
+conn = sqlite3.connect('quiz.db') # Warning: This file is created in the current directory
 conn.execute("CREATE TABLE topics (id INTEGER PRIMARY KEY, topic char(100) NOT NULL)")
 conn.execute("CREATE TABLE terms (id INTEGER PRIMARY KEY, term char(100) NOT NULL, definition char(100) NOT NULL)")
 conn.commit()
