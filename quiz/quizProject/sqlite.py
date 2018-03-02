839df960f3f4ae89a6fa4dc1b71ec76a0ff3f83d
Alienemperor <joshua.mckinnon.10@gmail.com>
2018-03-02 12:09:52 -0700

    db

--- quiz/quizProject/sqlite.py
+++ quiz/quizProject/sqlite.py
@@ -1,6 +1,6 @@
 import sqlite3
 conn = sqlite3.connect('quiz.db') # Warning: This file is created in the current directory
 #conn.execute("CREATE TABLE topics (id INTEGER PRIMARY KEY, topic char(100) NOT NULL)")
-conn.execute("DROP TABLE terms")
+#conn.execute("DROP TABLE terms")
 conn.execute("CREATE TABLE terms (id INTEGER PRIMARY KEY, topicid INTEGER, term char(100) NOT NULL, definition char(100) NOT NULL, FOREIGN KEY(topicid) REFERENCES topics(id))")
 conn.commit()
