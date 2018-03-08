03fde18fa74280630515a1c161d876eeff889f87
alienemperor <32971607+alienemperor@users.noreply.github.com>
2018-03-08 15:00:17 -0700

    josh

--- quiz/quizProject/quiz.py
+++ quiz/quizProject/quiz.py
@@ -94,11 +94,11 @@ def quiz_tests(no):
             resultquest.append(quest)
 
             for i in result:
-                if quest in i[0] and ans in i[1]:
+                if quest in i[0] and ans == i[1]:
                     realans.append(i[1])
                     success.append(True)
                     print(i[1], "true")
-                elif quest in i[0] and ans not in i[1]:
+                elif quest in i[0] and ans == i[1]:
                     realans.append(i[1])
                     success.append(False)
                     print(quest, i[1], "false")
