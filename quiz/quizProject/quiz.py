8d5c007bfffc5a36cdcb53ac3587534b3a7e5039
alienemperor <32971607+alienemperor@users.noreply.github.com>
2018-04-09 10:26:44 -0600

    .

--- quiz/quizProject/quiz.py
+++ quiz/quizProject/quiz.py
@@ -108,6 +108,7 @@ def quiz_tests(no):
         questions = random.sample(range(0, len(result)), numQ)
 
         return template('quiz', questions=questions, numQ=numQ, result=result, topic=topic, no=no)
+        print(numQ)
 
 
 run(host='192.168.100.222', port=8090, debug=True, reloader=True)
